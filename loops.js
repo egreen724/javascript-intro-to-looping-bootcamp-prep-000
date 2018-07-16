ea24b94695b75db5ee331af6eacd9f536fb7e3cb
Erin Green <erin.y.green@gmail.com>
2018-07-16 18:37:43 +0000

    automatically backed up by learn

--- loops.js
+++ loops.js
@@ -2,6 +2,6 @@ function forLoop(array) {
   for (let i = 0; i < 25; i++) {
     if (i === 1) {console.log("I am 1 strange loop.")}
     else (i != 1) {console.log(`"I am ${i} strange loops."`)}
-  }
   return array;
+  }
 }
